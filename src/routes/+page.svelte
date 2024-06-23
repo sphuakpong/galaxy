387226966c4d5603ad6b233ebd1a986c0b887a00
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-06-23 20:41:57 +0700

    copy code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,18 +1,147 @@
 <script lang="ts">
-  import * as dat from "lil-gui";
+  import * as THREE from "three";
+  import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+  import GUI from "lil-gui";
   import { onMount } from "svelte";
 
   onMount(() => {
     /**
+     * Debug
+     */
+    const gui = new GUI({
+      width: 300,
+      title: "Nice debug UI",
+      closeFolders: false,
+    });
+    // gui.close()
+    // gui.hide()
+    window.addEventListener("keydown", (event) => {
+      if (event.key == "h") gui.show(gui._hidden);
+    });
+
+    const debugObject = {
+      color: "#a778d8",
+    };
+
+    /**
      * Base
      */
-    // Debug
-    const gui = new dat.GUI();
-
     // Canvas
-    const canvas = document.querySelector("canvas.webgl") as HTMLElement;
+    const canvas = document.querySelector("canvas.webgl");
+
+    if (!canvas) {
+      return;
+    }
+
+    // Scene
+    const scene = new THREE.Scene();
+
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+    const material = new THREE.MeshBasicMaterial({ color: "#a778d8", wireframe: true });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    const cubeTweaks = gui.addFolder("Awesome cube");
+    // cubeTweaks.close()
+
+    cubeTweaks.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
+
+    cubeTweaks.add(mesh, "visible");
+
+    cubeTweaks.add(material, "wireframe");
+
+    cubeTweaks.addColor(debugObject, "color").onChange(() => {
+      material.color.set(debugObject.color);
+    });
+
+    debugObject.spin = () => {
+      gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+    };
+    cubeTweaks.add(debugObject, "spin");
+
+    debugObject.subdivision = 2;
+    cubeTweaks
+      .add(debugObject, "subdivision")
+      .min(1)
+      .max(20)
+      .step(1)
+      .onFinishChange(() => {
+        mesh.geometry.dispose();
+        mesh.geometry = new THREE.BoxGeometry(
+          1,
+          1,
+          1,
+          debugObject.subdivision,
+          debugObject.subdivision,
+          debugObject.subdivision
+        );
+      });
+
+    /**
+     * Sizes
+     */
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight,
+    };
+
+    window.addEventListener("resize", () => {
+      // Update sizes
+      sizes.width = window.innerWidth;
+      sizes.height = window.innerHeight;
+
+      // Update camera
+      camera.aspect = sizes.width / sizes.height;
+      camera.updateProjectionMatrix();
+
+      // Update renderer
+      renderer.setSize(sizes.width, sizes.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    });
+
+    /**
+     * Camera
+     */
+    // Base camera
+    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+    camera.position.x = 1;
+    camera.position.y = 1;
+    camera.position.z = 2;
+    scene.add(camera);
+
+    // Controls
+    const controls = new OrbitControls(camera, canvas as HTMLElement);
+    controls.enableDamping = true;
+
+    /**
+     * Renderer
+     */
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+    });
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+    /**
+     * Animate
+     */
+    const clock = new THREE.Clock();
+
+    const tick = () => {
+      const elapsedTime = clock.getElapsedTime();
+
+      // Update controls
+      controls.update();
+
+      // Render
+      renderer.render(scene, camera);
+
+      // Call tick again on the next frame
+      window.requestAnimationFrame(tick);
+    };
+
+    tick();
   });
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<canvas class="webgl" />
