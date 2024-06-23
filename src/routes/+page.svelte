41dd7885f7c6049323c7b9825128af098954fed3
sippakorn-phuakpong <cloneltorrer@gmail.com>
2024-06-23 19:06:13 +0700

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import * as dat from "lil-gui";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    /**
+     * Base
+     */
+    // Debug
+    const gui = new dat.GUI();
+
+    // Canvas
+    const canvas = document.querySelector("canvas.webgl") as HTMLElement;
+  });
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
